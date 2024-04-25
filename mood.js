87d8b1db845e80322fb919283182c6113d78dccf
my token <89487544+fmomodu@users.noreply.github.com>
2024-04-25 14:42:53 -0400

    Add files via upload

--- mood.js
+++ mood.js
@@ -1,38 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Get references to all mood buttons
   const moodBtns = document.querySelectorAll('.mood-btn');
 
-  // Add click event listener to each mood button
   moodBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-      // Get the selected mood from the data-mood attribute
-      const selectedMood = this.dataset.mood;
+    btn.addEventListener('click', function(event) {
+      event.preventDefault();
 
-      // Based on the selected mood, redirect to the respective HTML page
-      switch(selectedMood) {
-        case 'happy':
-          window.location.href = 'happy.html';
-          break;
-        case 'neutral':
-          window.location.href = 'neutral.html';
-          break;
-        case 'sad':
-          window.location.href = 'sad.html';
-          break;
-        case 'angry':
-          window.location.href = 'angry.html';
-          break;
-        case 'in love':
-          window.location.href = 'love.html';
-          break;
-        case 'confident':
-          window.location.href = 'confident.html';
-          break;
-        // Add more cases for additional mood options as needed
-        default:
-          // Handle default case if needed
-          break;
-      }
+      // Store the selected mood in sessionStorage
+      sessionStorage.setItem('selectedMood', this.dataset.mood);
+
+      // Redirect to the loading page
+      window.location.href = 'loading2.html';
     });
   });
 });
